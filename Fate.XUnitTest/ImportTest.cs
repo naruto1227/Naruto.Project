6b9c3b7dcd0f7749820963329cb0186511d2a071
海波 张 <1091675746@qq.com>
2019-06-03 18:00:58 +0800

    npoi

---
+++ Fate.XUnitTest/ImportTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using Fate.Common.NPOI;
+using System.IO;
+
+namespace Fate.XUnitTest
+{
+   public class ImportTest
+    {
+        [Fact]
+        public void test() {
+          var dt=  ImportHelper.ToDataTable(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "test.xls"),"result",0);
+        }
+    }
+}
--- Fate.XUnitTest/UnitTest1.cs
+++ Fate.XUnitTest/UnitTest1.cs
@@ -5,6 +5,9 @@ using Fate.Domain.Model.Entities;
 using System.Diagnostics;
 using Fate.Common.Ioc.Core;
 using Fate.Common.Repository.Mysql;
+using System.Data;
+using Fate.Common.NPOI;
+using System.IO;
 
 namespace Fate.XUnitTest
 {
@@ -13,6 +16,16 @@ namespace Fate.XUnitTest
         [Fact]
         public void Test1()
         {
+            DataTable dt = new DataTable();
+            dt.Columns.Add("列一");
+            for (int i = 0; i < 10; i++)
+            {
+                DataRow dr = dt.NewRow();
+                dr["列一"] = 1;
+                dt.Rows.Add(dr);
+            }
+
+            ExportHelper.ToExcel(dt,new string[] { "列一"},"result",Path.Combine(AppDomain.CurrentDomain.BaseDirectory,"test.xls"));
         }
     }
 }
