deb0440109ad96fbd266072da3721f1e541495c5
海波 张 <1091675746@qq.com>
2019-06-11 10:48:43 +0800

    单元测试

---
+++ Fate.XUnitTest/RedisTest.cs
@@ -0,0 +1,26 @@
+﻿using StackExchange.Redis;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Fate.XUnitTest
+{
+   public class RedisTest
+    {
+        [Fact]
+        public void test()
+        {
+            var redis = ConnectionMultiplexer.Connect("127.0.0.1");
+            ISubscriber subscriber = redis.GetSubscriber();
+            //订阅
+            subscriber.Subscribe("push", (chanel, msg) => {
+                Console.WriteLine(chanel);
+                Console.WriteLine(msg);
+            });
+            //发布
+            subscriber.Publish("push", "你好");
+
+        }
+    }
+}
