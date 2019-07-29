549e254c7cbf7e9ad99f18970972bac8c8a7d35a
海波 <1091675746@qq.com>
2019-07-29 15:35:56 +0800

    文件服务器动态创建文件夹

--- Fate.Common/Extensions/ServiceCollectionExtensions/FileUploadExtensions.cs
+++ Fate.Common/Extensions/ServiceCollectionExtensions/FileUploadExtensions.cs
@@ -5,6 +5,7 @@ using System.Text;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Options;
 using Fate.Common.Options;
+using System.IO;
 
 namespace Fate.Common.Extensions
 {
@@ -16,6 +17,16 @@ namespace Fate.Common.Extensions
         /// <returns></returns>
         public static IServiceCollection UseFileOptions(this IServiceCollection services, Action<FileUploadOptions> options)
         {
+            FileUploadOptions fileUploadOptions = new FileUploadOptions();
+            options?.Invoke(fileUploadOptions);
+
+            if (fileUploadOptions != null && !string.IsNullOrWhiteSpace(fileUploadOptions.UploadFilePath))
+            {
+                if (!Directory.Exists(fileUploadOptions.UploadFilePath))
+                {
+                    Directory.CreateDirectory(fileUploadOptions.UploadFilePath);
+                }
+            }
             return services.Configure(options);
         }
     }
