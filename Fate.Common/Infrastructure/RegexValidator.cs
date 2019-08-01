30c716cd9675dcb8f5620fe643facdb06172d0ef
海波 <1091675746@qq.com>
2019-08-01 16:11:34 +0800

    车牌号验证

--- Fate.Common/Infrastructure/RegexValidator.cs
+++ Fate.Common/Infrastructure/RegexValidator.cs
@@ -603,6 +603,15 @@ namespace Fate.Common.Infrastructure
             else
                 return false;
         }
+        /// <summary>
+        /// 验证车牌号
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns></returns>
+        public static bool IsLicenseNumber(string input) {
+            string pattern = "^(([京津沪渝冀豫云辽黑湘皖鲁新苏浙赣鄂桂甘晋蒙陕吉闽贵粤青藏川宁琼使领][A-Z](([0-9]{5}[DF])|([DF]([A-HJ-NP-Z0-9])[0-9]{4})))|([京津沪渝冀豫云辽黑湘皖鲁新苏浙赣鄂桂甘晋蒙陕吉闽贵粤青藏川宁琼使领][A-Z][A-HJ-NP-Z0-9]{4}[A-HJ-NP-Z0-9挂学警港澳使领]))$";
+            return IsMatch(input, pattern);
+        }
         #endregion
     }
 }
