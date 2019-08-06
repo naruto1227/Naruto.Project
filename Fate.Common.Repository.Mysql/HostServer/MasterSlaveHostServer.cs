09b61ee5011ec0360ca94c1a1333f43a4be42e2e
海波 <1091675746@qq.com>
2019-08-06 16:31:43 +0800

    更新后台服务

--- Fate.Common.Repository.Mysql/HostServer/MasterSlaveHostServer.cs
+++ Fate.Common.Repository.Mysql/HostServer/MasterSlaveHostServer.cs
@@ -12,42 +12,34 @@ namespace Fate.Common.Repository.Mysql.HostServer
 {
     internal class MasterSlaveHostServer : IHostedService
     {
-        /// <summary>
-        /// 应用程序的生命周期
-        /// </summary>
-        private IApplicationLifetime applicationLifetime;
 
         private ILogger<MasterSlaveHostServer> logger;
 
         private Timer timer;
 
-        public MasterSlaveHostServer(IApplicationLifetime _applicationLifetime, ILogger<MasterSlaveHostServer> _logger)
+        public MasterSlaveHostServer( ILogger<MasterSlaveHostServer> _logger)
         {
-            applicationLifetime = _applicationLifetime;
             logger = _logger;
         }
 
         public Task StartAsync(CancellationToken cancellationToken)
         {
-            applicationLifetime.ApplicationStarted.Register(Start);
-            applicationLifetime.ApplicationStopped.Register(Stopped);
-            applicationLifetime.ApplicationStopping.Register(Stopping);
             logger.LogInformation("启动服务");
+            //2分钟执行一次
+            timer = new Timer(Handler, null, TimeSpan.Zero, TimeSpan.FromMinutes(2));
             return Task.CompletedTask;
         }
 
         public Task StopAsync(CancellationToken cancellationToken)
         {
             logger.LogInformation("服务停止");
+            if (timer != null)
+            {
+                logger.LogInformation("停止定时器");
+                timer?.Change(Timeout.Infinite, 0);
+                timer.Dispose();
+            }
             return Task.CompletedTask;
-        }
-        /// <summary>
-        /// 启动
-        /// </summary>
-        private void Start()
-        {
-            //2分钟执行一次
-            timer = new Timer(Handler, null, TimeSpan.Zero, TimeSpan.FromMinutes(2));
         }
         /// <summary>
         /// 处理程序
@@ -57,18 +49,6 @@ namespace Fate.Common.Repository.Mysql.HostServer
         {
             logger.LogInformation("执行定时器验证从库服务器的状态");
             SlavePools.TimerHeartBeatCheck();
-        }
-        private void Stopped()
-        {
-            if (timer != null)
-            {
-                logger.LogInformation("停止定时器");
-                timer?.Change(Timeout.Infinite, 0);
-                timer.Dispose();
-            }
-        }
-        private void Stopping()
-        {
         }
     }
 }
