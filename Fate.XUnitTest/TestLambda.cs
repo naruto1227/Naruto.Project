3ee9b90643dfda02eb9cf59a4277a3d45e42a58e
也许所谓 <1091675746@qq.com>
2019-08-18 22:38:52 +0800

    测试

---
+++ Fate.XUnitTest/TestLambda.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Fate.XUnitTest
+{
+    public class TestLambda
+    {
+
+        [Fact]
+        public void test()
+        {
+            IServiceCollection serviceDescriptors = new ServiceCollection();
+            serviceDescriptors.AddSingleton<TestMethod>();
+            Dictionary<string, Action<string>> keyValuePairs = new Dictionary<string, Action<string>>();
+
+            var types = typeof(TestMethod);
+            var str = types.GetConstructors()[0];
+            var s = str.Invoke(null) as TestMethod;
+            keyValuePairs.Add("test", s.test1);
+            keyValuePairs["test"]("1");
+        }
+
+
+    }
+}
