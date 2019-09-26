1c720be9c2fc0104c303b06833fa8e8c7567887b
海波 <1091675746@qq.com>
2019-09-26 17:02:41 +0800

    添加一个实体映射公共设施层

---
+++ Fate.Common.Mapper/DefaultEntityMapper.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+
+namespace Fate.Common.Mapper
+{
+    public class DefaultEntityMapper : IEntityMapper
+    {
+        private readonly IMapper mapper;
+
+        public DefaultEntityMapper(IMapper _mapper)
+        {
+            mapper = _mapper;
+        }
+
+        /// <summary>
+        /// 传输一个对象
+        /// </summary>
+        /// <typeparam name="T">返回类型</typeparam>
+        /// <param name="soure">数据源</param>
+        /// <returns></returns>
+        public T MapperTo<T>(object soure) where T : class
+        {
+            return mapper.Map<T>(soure);
+        }
+        /// <summary>
+        /// 为已经存在的对象进行automapper
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="soure"></param>
+        /// <param name="result"></param>
+        /// <returns></returns>
+        public T MapperTo<T>(object soure, T result) where T : class
+        {
+            return (T)mapper.Map(soure, result, soure.GetType().UnderlyingSystemType, result.GetType());
+        }
+        /// <summary>
+        /// 传输一个集合
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="soure"></param>
+        /// <returns></returns>
+        public Task<List<T>> MapperToList<T>(object soure) where T : class
+        {
+            return Task.Run(() => mapper.Map<List<T>>(soure));
+        }
+    }
+}
---
+++ Fate.Common.Mapper/ServiceCollectionExtensions.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using AutoMapper;
+using System.Linq;
+using Fate.Common.Mapper;
+using Fate.Common.Mapper.Extensions;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    /// <summary>
+    /// automapper 依赖注入
+    /// </summary>
+    public static class ServiceCollectionExtensions
+    {
+        /// <summary>
+        /// 注册实体映射关系
+        /// </summary>
+        /// <param name="services"></param>
+        /// <param name="typesParams"></param>
+        /// <returns></returns>
+        public static IServiceCollection RegisterMapper(this IServiceCollection services, params Type[] typesParams)
+        {
+
+            services.AddTransient(typeof(IAutoInjectFactory), typeof(DefaultAutoInjectFactory));
+            services.AddTransient(typeof(IEntityMapper), typeof(DefaultEntityMapper));
+            return services.AddAutoMapper(item =>
+            {
+                using (var itemServices = services.BuildServiceProvider().CreateScope())
+                {
+                    //获取服务
+                    var autoInjectFactory = itemServices.ServiceProvider.GetRequiredService<IAutoInjectFactory>();
+                    //获取所有需要映射的类型对象
+                    var list = autoInjectFactory.GetFromAssemblys(typesParams);
+                    if (list != null && list.Count() > 0)
+                    {
+                        foreach (var itemOptions in list)
+                        {
+                            item.CreateMap(itemOptions.SoureType, itemOptions.TargetType).IgnoreAllExisting(itemOptions.IgnoreName).ReverseMap(itemOptions.ReverseMap);
+                        }
+                    }
+                }
+            }
+            , typeof(ServiceCollectionExtensions));
+        }
+    }
+}
