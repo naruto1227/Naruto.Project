b065bb1963335d6218863d67f304469d181622c5
也许所谓 <1091675746@qq.com>
2019-11-23 01:26:55 +0800

    注释

--- Fate.Common.Configuration/FateConfigurationInfrastructure.cs
+++ Fate.Common.Configuration/FateConfigurationInfrastructure.cs
@@ -6,10 +6,11 @@ using System.Text;
 
 namespace Fate.Common.Configuration
 {
+
     /// <summary>
     /// 张海波
     /// 2019-11-21 
-    /// 存储配置的key 可用于重新加载配置的时候
+    /// 存储配置的key 可用于重新加载配置的时候（暂时无用）
     /// </summary>
     internal static class FateConfigurationInfrastructure
     {
