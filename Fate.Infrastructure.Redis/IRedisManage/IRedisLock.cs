2e28eebd40d028077a2b08ffe60e9e470727cbca
海波 <1091675746@qq.com>
2019-12-10 16:32:39 +0800

    注释

--- Fate.Infrastructure.Redis/IRedisManage/IRedisLock.cs
+++ Fate.Infrastructure.Redis/IRedisManage/IRedisLock.cs
@@ -14,24 +14,24 @@ namespace Fate.Infrastructure.Redis.IRedisManage
     public interface IRedisLock : IRedisDependency
     {
         /// <summary>
-        /// 锁
+        /// 锁(过期时间内有其他的访问都为false)
         /// </summary>
         /// <param name="key">key </param>
         /// <param name="value">value</param>
         /// <param name="expiry">过期时间</param>
         /// <param name="flags"></param>
         /// <returns></returns>
-        bool Lock(string key, string value, TimeSpan expiry=default, CommandFlags flags = CommandFlags.None);
+        bool Lock(string key, string value, TimeSpan expiry = default, CommandFlags flags = CommandFlags.None);
 
         /// <summary>
-        /// 锁
+        /// 锁(过期时间内有其他的访问都为false)
         /// </summary>
         /// <param name="key"></param>
         /// <param name="value"></param>
         /// <param name="expiry"></param>
         /// <param name="flags"></param>
         /// <returns></returns>
-        Task<bool> LockAsync(string key, string value, TimeSpan expiry=default, CommandFlags flags = CommandFlags.None);
+        Task<bool> LockAsync(string key, string value, TimeSpan expiry = default, CommandFlags flags = CommandFlags.None);
         /// <summary>
         /// 释放锁
         /// </summary>
