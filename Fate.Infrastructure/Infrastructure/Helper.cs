2e97b5191a87bc1d15758ce3f3eec83452d77694
海波 <1091675746@qq.com>
2020-01-07 09:36:01 +0800

    基础设施

--- Fate.Infrastructure/Infrastructure/Helper.cs
+++ Fate.Infrastructure/Infrastructure/Helper.cs
@@ -3,18 +3,21 @@ using System.Collections.Generic;
 using System.Threading.Tasks;
 using System.Security.Cryptography;
 using System.Text;
+using System.Reflection;
+using System.Linq;
+
 namespace Fate.Infrastructure.Infrastructure
 {
     /// <summary>
     /// 帮助类
     /// </summary>
-    public class Helper
+    public static class Helper
     {
         /// 32位加密
         /// </summary>
         /// <param name="source"></param>
         /// <returns></returns>
-        public static string Get32MD5(string source)
+        public static string ToMD5(this string source)
         {
             using (MD5 md5Hash = MD5.Create())
             {
@@ -26,6 +29,42 @@ namespace Fate.Infrastructure.Infrastructure
                 }
                 string hash = sBuilder.ToString();
                 return hash.ToUpper();
+            }
+        }
+
+
+        /// <summary>
+        /// 实体映射
+        /// </summary>
+        /// <typeparam name="T">同步目标的类型</typeparam>
+        /// <param name="sourceObj">同步源对象</param>
+        /// <param name="targetObj">同步目标对象</param>
+        /// <param name="ignoreFileds">不需要同步的键值</param>
+        public static void AttributeSync<T>(this object sourceObj, ref T targetObj, params string[] ignoreFileds) where T : class
+        {
+            Type sourceType = sourceObj.GetType();
+            Type targetType = targetObj.GetType();
+            PropertyInfo[] sourceProperties = sourceType.GetProperties();
+            PropertyInfo[] targetProperties = targetType.GetProperties();
+
+            if (ignoreFileds == null)
+            {
+                ignoreFileds = new string[0];
+            }
+            foreach (PropertyInfo sourceProperty in sourceProperties)
+            {
+                if (ignoreFileds.Contains(sourceProperty.Name))
+                {
+                    continue;
+                }
+                foreach (PropertyInfo targetProperty in targetProperties)
+                {
+                    if (targetProperty.Name.Equals(sourceProperty.Name, StringComparison.CurrentCultureIgnoreCase) && sourceProperty.GetValue(sourceObj, null) != null)
+                    {
+                        targetProperty.SetValue(targetObj, Convert.ChangeType(sourceProperty.GetValue(sourceObj, null), targetProperty.PropertyType), null);
+                        break;
+                    }
+                }
             }
         }
     }
