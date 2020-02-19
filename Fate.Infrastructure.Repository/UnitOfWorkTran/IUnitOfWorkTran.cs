af962234cee63732d1e767e2dae56e2b0ae63983
也许所谓 <1091675746@qq.com>
2020-02-19 23:53:23 +0800

    加入工作单元事务批量提交接口（未完成）

---
+++ Fate.Infrastructure.Repository/UnitOfWorkTran/IUnitOfWorkTran.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Fate.Infrastructure.Repository.UnitOfWork
+{
+    /// <summary>
+    /// 张海波
+    /// 2020-02-19
+    /// 当前接口 用于多工作单元时 批量操作事务
+    /// </summary>
+    public interface IUnitOfWorkTran
+    {
+        /// <summary>
+        /// 开启事务
+        /// </summary>
+        /// <returns></returns>
+        void BeginTransaction();
+         
+        /// <summary>
+        /// 异步开始事务
+        /// </summary>
+        /// <returns></returns>
+        Task BeginTransactionAsync(CancellationToken cancellationToken = default);
+
+        /// <summary>
+        /// 提交事务
+        /// </summary>
+        void CommitTransaction();
+
+        /// <summary>
+        /// 提交事务
+        /// </summary>
+        Task CommitTransactionAsync(CancellationToken cancellationToken = default);
+        /// <summary>
+        /// 事务回滚
+        /// </summary>
+        void RollBackTransaction();
+
+        /// <summary>
+        /// 事务回滚
+        /// </summary>
+        Task RollBackTransactionAsync(CancellationToken cancellationToken = default);
+    }
+}
---
+++ Fate.Infrastructure.Repository/UnitOfWorkTran/UnitOfWorkTran.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Fate.Infrastructure.Repository.UnitOfWork
+{
+    public class UnitOfWorkTran : IUnitOfWorkTran
+    {
+        public void BeginTransaction()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task BeginTransactionAsync(CancellationToken cancellationToken = default)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CommitTransaction()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task CommitTransactionAsync(CancellationToken cancellationToken = default)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RollBackTransaction()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task RollBackTransactionAsync(CancellationToken cancellationToken = default)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
