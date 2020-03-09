7bd1cf704658aa6cb4c916b95729edfe7f9d64d1
也许所谓 <1091675746@qq.com>
2020-03-10 00:54:12 +0800

    更改基础实体的类型

--- Fate.Infrastructure.BaseMongo.Model/IMongoEntity.cs
+++ Fate.Infrastructure.BaseMongo.Model/IMongoEntity.cs
@@ -8,7 +8,7 @@ namespace Fate.Infrastructure.BaseMongo.Model
     /// <summary>
     /// 张海波
     /// 2019-12-1
-    /// mongo实体
+    /// 使用mongodb自带的ObjectId作为主键
     /// </summary>
     public abstract class IMongoEntity : IMongoModel
     {
@@ -17,7 +17,7 @@ namespace Fate.Infrastructure.BaseMongo.Model
         /// mongodb的主键id ,当实体中不存在Id或者id或者_id 字段的时候
         /// 此实体需要继承当前接口
         /// </summary>
-        public virtual ObjectId Id { get; set; }
+        public virtual string Id { get; set; } = ObjectId.GenerateNewId().ToString();
     }
     /// <summary>
     /// 实体模型
